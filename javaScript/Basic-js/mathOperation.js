1b1b20d0f83d07d3e8a46957a1c917a67ba2083b
sadat <sadat.himel@gmail.com>
2020-07-12 11:44:54 +0600

    absolute ,round,floor,ceil,random

--- javaScript/Basic-js/mathOperation.js
+++ javaScript/Basic-js/mathOperation.js
@@ -1,23 +1,44 @@
-// var price1 = 25;
-// var price2 = 35;
-// price2++;
-// var total = price2 + price1;
-// var sub = price2 - price1;
-// var mul = price2 * price1;
-// var divi = price2 / price1;
-// var modu = price2 % price1;
-// console.log(total);
-// console.log(sub);
-// console.log(mul);
-// console.log(divi);
-// console.log(modu);
+// // var price1 = 25;
+// // var price2 = 35;
+// // price2++;
+// // var total = price2 + price1;
+// // var sub = price2 - price1;
+// // var mul = price2 * price1;
+// // var divi = price2 / price1;
+// // var modu = price2 % price1;
+// // console.log(total);
+// // console.log(sub);
+// // console.log(mul);
+// // console.log(divi);
+// // console.log(modu);
 
-// var price1 = 25;
-// var name = 'alu';
-// var result = name + price1;
-// console.log(result);
+// // var price1 = 25;
+// // var name = 'alu';
+// // var result = name + price1;
+// // console.log(result);
 
-var firstName = 'Justin';
-var lastName = 'Bieber';
-var fullName = firstName + ' ' + lastName;
-console.log(fullName)
+// var firstName = 'Justin';
+// var lastName = 'Bieber';
+// var fullName = firstName + ' ' + lastName;
+// console.log(fullName)
+
+// Absolute Number
+// var number = -5;
+// var absoluteNumber = Math.abs(number);
+// console.log(absoluteNumber)
+// var number = 5.5545;
+// var number = 5.4999;
+// var round = Math.round(number);
+// console.log(round);
+
+//  var number = 5.5545;
+ var number = 5.001;
+ var ceilNumber = Math.ceil(number)
+ var floorNumber = Math.floor(number);
+ var randonNumber = Math.random(number) *100;
+ var result = Math.round(randonNumber)
+ console.log(ceilNumber) 
+ console.log(floorNumber)
+ console.log(randonNumber)
+ console.log(result);
+ 
