5cd7d4b9dcfbb01e07bafb4d073ddd64d8853f69
sadat <sadat.himel@gmail.com>
2020-07-12 10:04:20 +0600

    variable learn

---
+++ javaScript/Basic-js/hello.js
@@ -0,0 +1,26 @@
+var BananaPrice = 12;
+console.log(typeof BananaPrice);
+var seenAfte = 21;
+console.log(typeof seenAfte);
+
+var name = "KolimUddin Zuckarburrg"
+console.log(typeof name)
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+var gureKe = "fan"
+var alodeyKe ="light"
+console.log(gureKe)
+console.log(alodeyKe)
+
+var bottol = 2;
+var mbl = 1;
+console.log(bottol);
+console.log(mbl);
+
+var isGhum = true;
+var iskothabolo = false;
+console.log(isGhum);
+console.log(iskothabolo);
---
+++ javaScript/Basic-js/variable.js
@@ -0,0 +1,5 @@
+var myname ='Hero Alom';
+myname = 'himel'
+myname = 'sadad'
+myname = 'salma'
+console.log(myname)
