7c4a8a3af92b7c78bb7356f07c07f6e4ea3955d1
sadat <sadat.himel@gmail.com>
2020-07-12 11:15:04 +0600

    learn mathOperation

---
+++ javaScript/Basic-js/mathOperation.js
@@ -0,0 +1,23 @@
+// var price1 = 25;
+// var price2 = 35;
+// price2++;
+// var total = price2 + price1;
+// var sub = price2 - price1;
+// var mul = price2 * price1;
+// var divi = price2 / price1;
+// var modu = price2 % price1;
+// console.log(total);
+// console.log(sub);
+// console.log(mul);
+// console.log(divi);
+// console.log(modu);
+
+// var price1 = 25;
+// var name = 'alu';
+// var result = name + price1;
+// console.log(result);
+
+var firstName = 'Justin';
+var lastName = 'Bieber';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName)
