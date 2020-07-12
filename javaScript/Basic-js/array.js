0024330d65e068e9b4d8d7bd98714a1ea6afe0b6
sadat <sadat.himel@gmail.com>
2020-07-13 02:09:46 +0600

    array.slice

--- javaScript/Basic-js/array.js
+++ javaScript/Basic-js/array.js
@@ -1,28 +1,41 @@
-var firendsAge = [15, 17, 14, 16];
-console.log(firendsAge);
-var sonaliAge = firendsAge[2];
-firendsAge[1] = 21;
-console.log(firendsAge);
+// var firendsAge = [15, 17, 14, 16];
+// console.log(firendsAge);
+// var sonaliAge = firendsAge[2];
+// firendsAge[1] = 21;
+// console.log(firendsAge);
 
-var position = firendsAge.indexOf(144);
-console.log(position);
+// var position = firendsAge.indexOf(144);
+// console.log(position);
 
-// var name = ["sadat", "himel", "umme", "salma"];
-// console.log(name);
-// name[1] = "sadat himel";
-// console.log(name);
-// var salmaIndex = name.indexOf["salma"];
-// console.log(salmaIndex);
-console.log(firendsAge.length);
-console.log(firendsAge)
+// // var name = ["sadat", "himel", "umme", "salma"];
+// // console.log(name);
+// // name[1] = "sadat himel";
+// // console.log(name);
+// // var salmaIndex = name.indexOf["salma"];
+// // console.log(salmaIndex);
+// console.log(firendsAge.length);
+// console.log(firendsAge)
 
-firendsAge.push(15);
-firendsAge.push(19);
-console.log(firendsAge)
-firendsAge.pop();
-console.log(firendsAge);
-console.log(firendsAge.length);
-firendsAge.shift()
-console.log(firendsAge);
-firendsAge.unshift(2000);
-console.log(firendsAge);
+// firendsAge.push(15);
+// firendsAge.push(19);
+// console.log(firendsAge)
+// firendsAge.pop();
+// console.log(firendsAge);
+// console.log(firendsAge.length);
+// firendsAge.shift()
+// console.log(firendsAge);
+// firendsAge.unshift(2000);
+// console.log(firendsAge);
+// var teaLine = ['palam','kalam','salam','balam']
+// teaLine.push('Jalam')
+// console.log(teaLine);
+// teaLine.pop();
+// console.log(teaLine);
+// teaLine.unshift('Palam')
+// console.log(teaLine);
+// teaLine.shift()
+// console.log(teaLine)
+
+var teaLine = ['palam','kalam','salam','balam','golam','khalam']
+var part = teaLine.slice(2);
+console.log(part)
