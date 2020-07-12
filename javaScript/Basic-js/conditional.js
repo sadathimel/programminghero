2cb0423e3967d8e975d578b7e08bd41eb0696ddd
sadat <sadat.himel@gmail.com>
2020-07-12 12:15:31 +0600

    condition if-else,else-if learning

--- javaScript/Basic-js/conditional.js
+++ javaScript/Basic-js/conditional.js
@@ -1,7 +1,23 @@
-var biscuitPrice = 19;
-if(biscuitPrice != 10){
-    console.log("i will eat the biscuits");
+// var biscuitPrice = 19;
+// if(biscuitPrice != 10){
+//     console.log("i will eat the biscuits");
+// }
+// else{
+//     console.log("Khali cha is better!");
+// }
+var jobPaiso = true;
+var savingAmount = 5000;
+
+// if(jobPaiso == true && savingAmount > 200000){
+//     console.log("Aso tomar patri khuji");
+// }else {
+//     console.log("Tor kopale biye nai");
+// }
+if(jobPaiso == true && savingAmount > 200000){
+    console.log("Aso tomar patri khuji");
+}else if(jobPaiso == true) {
+    console.log("ahcca koi din por dekhbo");
 }
 else{
-    console.log("Khali cha is better!");
+    console.log("Tor kopale biye nai");
 }
