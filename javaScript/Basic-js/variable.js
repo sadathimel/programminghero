465881a92f7d23d0081728522c04e273f1a48f2f
sadat <sadat.himel@gmail.com>
2020-07-12 10:16:58 +0600

    learn indexOf string

--- javaScript/Basic-js/variable.js
+++ javaScript/Basic-js/variable.js
@@ -3,3 +3,14 @@ myname = 'himel'
 myname = 'sadad'
 myname = 'salma'
 console.log(myname)
+
+var name = `sadat himel`
+var lastName = 'hossen himel'
+var promise = "I promise i will HARD to become a programmer";
+var promiseLowerCase = promise.toLowerCase();
+console.log(promiseLowerCase);
+var promiseUpperCase = promise.toUpperCase();
+console.log(promiseUpperCase)
+var promiseIndexOf = promise.indexOf('will')
+console.log(promiseIndexOf)
+console.log(promise.indexOf('hard'));
