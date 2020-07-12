79437ccf580d06d7d82e02cdb7bdd79c5c5fcd9e
sadat <sadat.himel@gmail.com>
2020-07-12 10:34:26 +0600

    string split

--- javaScript/Basic-js/variable.js
+++ javaScript/Basic-js/variable.js
@@ -6,7 +6,7 @@ console.log(myname)
 
 var name = `sadat himel`
 var lastName = 'hossen himel'
-var promise = "I promise i will HARD to become a programmer";
+var promise = "I promise I will HARD to become a programmer";
 var promiseLowerCase = promise.toLowerCase();
 console.log(promiseLowerCase);
 var promiseUpperCase = promise.toUpperCase();
@@ -14,3 +14,4 @@ console.log(promiseUpperCase)
 var promiseIndexOf = promise.indexOf('will')
 console.log(promiseIndexOf)
 console.log(promise.indexOf('hard'));
+console.log(promise.split(''))
