1e7a899a1fdb0f3f5d825107dd0df4491f612243
sadat <sadat.himel@gmail.com>
2020-07-12 10:53:18 +0600

    learn javascript number

---
+++ javaScript/Basic-js/number.js
@@ -0,0 +1,13 @@
+// var number1 = 25;
+// var number2 = '25.5';
+// // number2 = parseInt(number2);
+// // number2 = +number2;
+// // console.log(number1 + number2)
+// number1 = '' + number1;
+// console.log(number1);
+
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+total = total.toFixed(1)
+console.log(total);
