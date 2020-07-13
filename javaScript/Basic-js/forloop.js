5cf3748306caa241db9e277ac8b473582e79c987
sadat <sadat.himel@gmail.com>
2020-07-13 21:44:02 +0600

    javascript switch.js

---
+++ javaScript/Basic-js/forloop.js
@@ -0,0 +1,11 @@
+// var nums = [55, 66, 77, 88, 99, 11, 44]
+// for(var i = 0; i < nums.length; i++){
+//     var element= nums[i];
+//     console.log(element)
+// }
+
+var firends = ['himel', 'salma', 'opel', 'sadat','bappy']
+for (let index = 0; index < firends.length; index++) {
+    const element = firends[index];
+    console.log(element)
+}
---
+++ javaScript/Basic-js/switch.js
@@ -0,0 +1,13 @@
+num = 7;
+switch (num) {
+  case 1000:
+    console.log("I am log");
+    break;
+  case 100:
+    console.log(" I am 100");
+    break;
+  case 5:
+    console.log("i am 5");
+  default:
+    console.log("I dont know who you are");
+}
