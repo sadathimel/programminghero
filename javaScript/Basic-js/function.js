479a00b86359c332f454f175e76ce97e37dea91a
sadat <sadat.himel@gmail.com>
2020-07-13 22:35:46 +0600

    learn function

--- javaScript/Basic-js/function.js
+++ javaScript/Basic-js/function.js
@@ -1,5 +1,20 @@
-function sayLoveYou(){
-    console.log('Jan o baby');
-    console.log("Billy baby... koi tumi");
+// function sayLoveYou(){
+//     console.log('Jan o baby');
+//     console.log("Billy baby... koi tumi");
+// }
+// sayLoveYou();
+// function doubleIt(num){
+//     var result = num * 2;
+//     return result;
+// }
+// var first = doubleIt(5)
+// var second = doubleIt(50)
+// var total = first + second;
+// console.log(total);
+
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
 }
-sayLoveYou();
+var sum = add(15, 17)
+console.log(sum);
