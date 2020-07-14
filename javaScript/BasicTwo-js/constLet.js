9cb87b2ac140d21c42dec83791574644451a482e
sadat <sadat.himel@gmail.com>
2020-07-14 23:36:16 +0600

    const learning

---
+++ javaScript/BasicTwo-js/constLet.js
@@ -0,0 +1,11 @@
+var name = 'Dab';
+if(name.length > 4) {
+    name ='Juvu';
+}
+console.log(name)
+let age = 15;
+age = 16;
+age = 17;
+console.log(age);
+const country = 'Bangladesh';
+console.log(country)
