e55f10decca3e6d16442e0b183785b7e7a9e3945
sadat <sadat.himel@gmail.com>
2020-07-14 22:24:36 +0600

    function use for inch to feet

---
+++ javaScript/BasicTwo-js/inchToFeet.js
@@ -0,0 +1,10 @@
+var inchToFeet = function(inch){
+   var feet = inch/12
+   return feet;
+}
+  var nanaFeet = inchToFeet(156)
+  console.log(nanaFeet)
+  var naniFeet = inchToFeet(288);
+  console.log(naniFeet);
+  var dadiFeet = inchToFeet(300)
+  console.log(dadiFeet)
