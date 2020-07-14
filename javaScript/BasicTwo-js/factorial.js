a0b997073c936376b14fbaa64e30b7630a08ae60
sadat <sadat.himel@gmail.com>
2020-07-15 00:50:28 +0600

    factorial with loop

--- javaScript/BasicTwo-js/factorial.js
+++ javaScript/BasicTwo-js/factorial.js
@@ -9,12 +9,37 @@
 //     console.log(i, factorial)
 // }
 
-function factorial (n){
-    var factorial = 1;
-    for(var i = 1; i <= n; i++) {
-        factorial = factorial * i;
+// factorial for-loop
+
+// function factorial (n){
+//     var factorial = 1;
+//     for(var i = 1; i <= n; i++) {
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+// var factorial20 = factorial(20)
+// console.log(factorial20)
+
+// factorial while - loop
+// var i = 1;
+// var factorial = 1;
+// while(i<=10){
+//     factorial = factorial * i;
+//     // console.log(i, factorial);
+//     i++;
+// }
+// console.log(factorial)
+
+function factorial(n){
+    var i = 1;
+    var fact = 1;
+    while(i <= n) {
+        fact = fact * i;
+        i++;
     }
-    return factorial;
+    return fact;
+
 }
-var factorial20 = factorial(20)
-console.log(factorial20)
+var result = factorial(10);
+console.log(result);
