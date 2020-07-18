7903c141cf3adedb4711851aca51d964a53dcf49
sadat <sadat.himel@gmail.com>
2020-07-19 00:11:52 +0600

    practice some veriable

---
+++ javaScript/bonus.js/bonus.js
@@ -0,0 +1,11 @@
+let number = 59;
+number = 60;
+const name = "Asad";
+console.log(number);
+
+// var marks = [89, 97, 89, 68, 94];
+
+// for (var i = 0; i < marks.length; i++) {
+//     var element = marks[i];
+//     console.log(element);
+}
