4b97315a4e06510f8eda06a2844c61be6cd0a225
sadat <sadat.himel@gmail.com>
2020-07-19 00:13:29 +0600

    practice array

--- javaScript/bonus.js/bonus.js
+++ javaScript/bonus.js/bonus.js
@@ -1,11 +1,11 @@
-let number = 59;
-number = 60;
-const name = "Asad";
-console.log(number);
+// let number = 59;
+// number = 60;
+// const name = "Asad";
+// console.log(number);
 
-// var marks = [89, 97, 89, 68, 94];
+var marks = [89, 97, 89, 68, 94];
 
-// for (var i = 0; i < marks.length; i++) {
-//     var element = marks[i];
-//     console.log(element);
+for (var i = 0; i < marks.length; i++) {
+    var element = marks[i];
+    console.log(element);
 }
