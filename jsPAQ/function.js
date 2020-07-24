20e0761b9ab955ac9d7c617bbddd5a917762186b
sadat <sadat.himel@gmail.com>
2020-07-25 02:25:09 +0600

    function use

---
+++ jsPAQ/function.js
@@ -0,0 +1,22 @@
+
+
+function evenify(num) {
+  if (num % 2 == 0) {
+    console.log(num, ": is even number");
+  } else {
+    console.log(num * 2, ": is odd number");
+  }
+}
+
+function evenify_all(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    evenify(num);
+  }
+}
+
+nums = [5, 12, 89, 45, 18, 8];
+evenify_all(nums)
+
+friends_age = [13, 17, 19, 20, 18];
+evenify_all(friends_age);
