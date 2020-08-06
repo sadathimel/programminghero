0ed812c87f1b4c711f92c3e264e20af58e0effd2
sadat <sadat.himel@gmail.com>
2020-08-07 02:11:07 +0600

    IIFF

---
+++ javaScript/functionGeneral.js/functionGeneral.js
@@ -0,0 +1,19 @@
+// function addTodo(task){
+//     const newTaskElement = document.createElement('li');
+// }
+
+// var name = 'Rased';
+// function addUser(){
+//     var romanticName = "Jantoos"
+//     console.log(name)
+// }
+// console.log(romanticName)
+
+
+// addUser()
+
+// IIFF
+(function () {
+  var romanticName = "Jantoos";
+  console.log(romanticName);
+})();
